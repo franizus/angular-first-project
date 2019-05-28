e0c33ebd17da9b7c56bfc3fbdb512c806ba81406
Francisco Izurieta <franizus@live.com>
2019-05-28 10:08:54 -0500

    the basics

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is a simple test',
+      'https://img.taste.com.au/ITgbQUXM/w643-h428-cfill-q90/taste/2016/11/butter-chicken-with-naan-81484-1.jpeg'
+    )
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
+}
