1c9be15870bcdcc21bba8a1f0fc61a4795fb4f93
Francisco Izurieta <franizus@live.com>
2019-06-10 14:01:54 -0500

    Forms

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,18 @@
-import { Recipe } from './../recipe.model';
-import { Component, OnInit } from '@angular/core';
-import { RecipeService } from '../recipe.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+
+import { RecipeService } from '../recipe.service';
+import { Recipe } from './../recipe.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(
     private recipeService: RecipeService,
@@ -18,7 +21,16 @@ export class RecipeListComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    this.subscription = this.recipeService.recipesChanged.subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+      }
+    );
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   onNewRecipe() {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -3,9 +3,12 @@ import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+  idCount = 3;
   private recipes: Recipe[] = [
     new Recipe(
       1,
@@ -37,5 +40,34 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    recipe.id = this.idCount;
+    this.idCount++;
+    this.recipes.push(recipe);
+    this.emitRecipess();
+  }
+
+  updateRecipe(id: number, newRecipe: Recipe) {
+    const index = this.getRecipeIndex(id);
+    this.recipes[index] = newRecipe;
+    this.emitRecipess();
+  }
+
+  getRecipeIndex(id: number) {
+    return this.recipes.findIndex((recipe: Recipe) => {
+      return recipe.id == id;
+    });
+  }
+
+  deleteRecipe(id: number) {
+    const index = this.getRecipeIndex(id);
+    this.recipes.splice(index, 1);
+    this.emitRecipess();
+  }
+
+  emitRecipess() {
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
